d3c804eaba49555cb048d71478e3073e72e43f70
tinan272 <mnguyen354@gatech.edu>
2024-02-23 16:48:59 -0500

    add search bar, material ui

---
+++ coffee_and_coding/app/components/GoogleMap.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const GoogleMap = ({ searchValue }) => {
+    return (
+        <div id="map">
+            <iframe
+                title={`Map`}
+                width="900"
+                height="500"
+                frameBorder="0"
+                style={{ border: 0 }}
+                referrerPolicy="no-referrer-when-downgrade"
+                src={`https://www.google.com/maps/embed/v1/search?key=${process.env.NEXT_PUBLIC_API_Key}&q=${searchValue}`}
+                allowFullScreen
+            >
+            </iframe>
+        </div>
+    );
+}
+
+GoogleMap.propTypes = {
+    searchValue: PropTypes.string.isRequired,
+};
