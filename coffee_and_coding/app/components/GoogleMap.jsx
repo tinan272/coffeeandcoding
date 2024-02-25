2a7a6ff4b2151436976a250900de614ac83243eb
tinan272 <mnguyen354@gatech.edu>
2024-02-25 15:00:29 -0500

    switch to react-google-map

--- coffee_and_coding/app/components/GoogleMap.jsx
+++ coffee_and_coding/app/components/GoogleMap.jsx
@@ -1,28 +1,26 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { GoogleMap, Marker, LoadScript } from '@react-google-maps/api';
+import React from "react";
+import PropTypes from "prop-types";
+import { GoogleMap, Marker, LoadScript } from "@react-google-maps/api";
 
 const MapContainer = ({ searchValue }) => {
-  const defaultCenter = { lat: 40.7128, lng: -74.0060 }; // Default center coordinates
+    const defaultCenter = { lat: 40.7128, lng: -74.006 }; // Default center coordinates
 
-  return (
-    <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_API_Key}>
-      <GoogleMap
-        mapContainerStyle={{ width: '100%', height: '500px' }}
-        center={defaultCenter}
-        zoom={10}
-      >
-        {/* Marker component */}
-        <Marker position={defaultCenter} title="Your Marker Title" />
-
-      </GoogleMap>
-    </LoadScript>
-  );
-}
-
+    return (
+        <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_API_Key}>
+            <GoogleMap
+                mapContainerStyle={{ width: "100%", height: "500px" }}
+                center={defaultCenter}
+                zoom={10}
+            >
+                {/* Marker component */}
+                <Marker position={defaultCenter} title="Your Marker Title" />
+            </GoogleMap>
+        </LoadScript>
+    );
+};
 
 MapContainer.propTypes = {
-  searchValue: PropTypes.string.isRequired,
+    searchValue: PropTypes.string.isRequired,
 };
 
 export default MapContainer;
