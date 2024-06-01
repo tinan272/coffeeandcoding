94254852a1233a3ffd2f4845e6732367aa945056
tinan272 <mnguyen354@gatech.edu>
2024-06-01 14:22:06 -0400

    display shop info

--- coffee_and_coding/app/frontend/components/ShopListDisplay.jsx
+++ coffee_and_coding/app/frontend/components/ShopListDisplay.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import List from "@mui/material/List";
 import Paper from "@mui/material/Paper";
 import { ListSubheader } from "@mui/material";
@@ -7,7 +7,6 @@ import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemText from "@mui/material/ListItemText";
 import { Search } from "./Search.jsx";
-import Stack from "@mui/material/Stack";
 import { SelectMulti } from "./SelectMulti.jsx";
 import Box from "@mui/material/Box";
 import axios from "axios";
@@ -37,26 +36,28 @@ export const ShopListDisplay = ({}) => {
 
     const [searchValue, setSearchValue] = useState("");
     const names = ["Atlanta", "Buckhead", "Marietta", "Roswell"];
-    const scores = [1, 2, 3, 4, 5];
+    const cost = ["$", "$$", "$$$"];
 
     const [areaName, setAreaName] = useState([]);
-    const [cafeScore, setCafeScore] = useState([]);
-    const [cafeInfo, setCafe] = useState([]);
+    const [cafeCost, setcafeCost] = useState([]);
+    const [cafeInfo, setCafeInfo] = useState([]);
 
     useEffect(() => {
-
-    })
+        getCafeInfo().then((cafeData) => {
+            setCafeInfo(cafeData);
+        });
+    }, []);
 
     // filter based on the cafe name that is set by Search comp.
-    const filteredShops = staticShops.filter((shop) => {
+    const filteredShops = cafeInfo.filter((shop) => {
         const matchesName = shop.name
             .toLowerCase()
             .includes(searchValue.toLowerCase());
         const matchesArea =
-            areaName.length === 0 || areaName.includes(shop.area); // === 0 shows everything if no filter set
-        const matchesScore =
-            cafeScore.length === 0 || cafeScore.includes(shop.score);
-        return matchesName && matchesArea && matchesScore;
+            areaName.length === 0 || shop.address.includes(areaName); // === 0 shows everything if no filter set
+        const matchesCost =
+            cafeCost.length === 0 || cafeCost.includes(shop.cost);
+        return matchesName && matchesCost && matchesArea;
     });
 
     return (
@@ -71,25 +72,25 @@ export const ShopListDisplay = ({}) => {
                         setter={setAreaName}
                     />
                     <SelectMulti
-                        list={scores}
-                        labelName={"Scores"}
-                        filterValue={cafeScore}
-                        setter={setCafeScore}
+                        list={cost}
+                        labelName={"Cost"}
+                        filterValue={cafeCost}
+                        setter={setcafeCost}
                     />
                 </Box>
                 <List>
                     <ListSubheader>Coffee Shops</ListSubheader>
 
-                    {filteredShops.map((shop, index) => (
-                        <ListItemButton>
+                    {filteredShops.map((cafe, index) => (
+                        <ListItemButton key={cafe.name}>
                             <ListItem
-                                key={index}
                                 sx={{ borderBottom: 1, borderColor: "divider" }}
                             >
                                 <ListItemText
-                                    primary={shop.name}
-                                    secondary={shop.description}
+                                    primary={cafe.name}
+                                    secondary={cafe.address}
                                 />
+                                {cafe.cost}
                             </ListItem>
                         </ListItemButton>
                     ))}
@@ -102,10 +103,23 @@ export const ShopListDisplay = ({}) => {
         try {
             const response = await axios.get("http://localhost:8080/cafe_api");
             const cafeData = response.data;
-            const cafes = cafeData.map((cafe) => ({
-                name: cafe.name,
-                address: cafe.address
-            }))
+            console.log("Getting cafe data....");
+            console.log(cafeData);
+            const cafes = cafeData.map((cafe) => {
+                console.log("Cost:", cafe.Cost);
+                return {
+                    name: cafe.Name,
+                    address: cafe.Address,
+                    parking: cafe.Parking,
+                    cost: cafe.Cost,
+                };
+            });
+            console.log("getting cafe info...");
+            console.log(cafes);
+            return cafes;
+        } catch (error) {
+            console.log("error fetching cafe: ", error);
+            return [];
         }
     }
 };
