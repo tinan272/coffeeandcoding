043cde5a0f5df03d19db532173bfd7a59aceee96
Amal <achaudry6@gatech.edu>
2024-06-01 12:15:08 -0400

    fixed db connection

--- coffee_and_coding/app/backend/server.js
+++ coffee_and_coding/app/backend/server.js
@@ -9,6 +9,11 @@ async function startServer() {
         app.listen(8080, () => {
             console.log("server running on port 8080");
         });
+
+        const database = await client.db("coffee_shop_data");
+        const collection = await database.collection("coffee_info");
+        const documents = await collection.find().toArray();
+        console.log("Retrieved documents:", documents);
     } catch (error) {
         console.log("Error starting server", error);
     }
