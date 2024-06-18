279dd0b006b6b21ec85d5e084a70d155bb1878e7
tinan272 <mnguyen354@gatech.edu>
2024-06-18 13:31:16 -0400

    fix merge conflicts

--- coffee_and_coding/app/frontend/components/DisplayOptions.jsx
+++ coffee_and_coding/app/frontend/components/DisplayOptions.jsx
@@ -1,24 +1,39 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemText from "@mui/material/ListItemText";
 import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
 import { SelectMulti } from "./SelectMulti";
+import { Button, ListSubheader, ThemeProvider } from "@mui/material";
+import axios from "axios";
+import theme from "./theme.jsx";
+import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
 
-const filterOptions = { 0: "Area", 1: "Cost", 2: "Rating", 3: "Parking" };
+const filterOptions = {
+    cities: "Area",
+    costs: "Cost",
+    ratings: "Rating",
+    parking: "Parking",
+};
 const sortOptions = { 0: "Cost", 1: "Rating" };
 
 export const DisplayOptions = ({
-    type,
-    openView,
-    handleClose,
-    setSelectedFilterOptions,
-    setSelectedSortOption,
+    type, // sort or filter
+    openView, // open select view
+    handleClose, // close view
+    setters, // setters for cities, cost, rating, parking
+    onClear, // clear all setters + select multi
+    selectedFilterValues, // "$$", "Atlanta" if those were selecte
+    selectedSortValue, // "cost" or "rating"
+    setSelectedSortValue, // stores chosen sort value
 }) => {
     const [openMultiView, setOpenMultiView] = useState(false);
     const [selectedType, setSelectedType] = useState(null);
+    const { cities, costs, ratings, parking } = selectedFilterValues;
+    const [clearMulti, setClearMulti] = useState(false);
 
     const handleOpenMultiView = (selectedType) => {
         setSelectedType(selectedType);
@@ -27,6 +42,31 @@ export const DisplayOptions = ({
 
     const onCloseMultiView = () => {
         setOpenMultiView(false);
+    };
+
+    const handleOnClear = () => {
+        onClear();
+        setClearMulti((prev) => !prev);
+    };
+
+    const getSelectedValues = (key) => {
+        if (selectedFilterValues.hasOwnProperty(key)) {
+            return selectedFilterValues[key].join(", ");
+        } else {
+            return "";
+        }
+    };
+
+    const handleOpenSortOrFilter = (key) => {
+        if (type === 1) {
+            handleOpenMultiView(key);
+        } else {
+            if (key == selectedSortValue) {
+                setSelectedSortValue(null);
+            } else {
+                setSelectedSortValue(key);
+            }
+        }
     };
 
     const renderOptions = (optionsDict) => {
@@ -39,13 +79,7 @@ export const DisplayOptions = ({
                     boxShadow: 1,
                     paddingBottom: "1rem",
                 }}
-                onClick={() => {
-                    {
-                        type
-                            ? handleOpenMultiView(key)
-                            : setSelectedSortOption(key);
-                    }
-                }}
+                onClick={() => handleOpenSortOrFilter(key)}
                 key={key}
             >
                 <ListItemText
@@ -55,12 +89,17 @@ export const DisplayOptions = ({
                         paddingLeft: "1rem",
                     }}
                     primary={option}
+                    secondary={getSelectedValues(key)} // Display selected values here
                 />
+                {!type &&
+                    selectedSortValue === key && ( //only render check if type == sort
+                        <Box>
+                            <CheckCircleOutlineIcon fontSize="medium" />
+                        </Box>
+                    )}
             </ListItemButton>
         ));
     };
-
-    // const { onClose, selectedValue, open } = props;
 
     return (
         <div>
@@ -92,20 +131,27 @@ export const DisplayOptions = ({
                                 : renderOptions(sortOptions)}
                         </ListItem>
                     </List>
+                    {type ? (
+                        <ThemeProvider theme={theme}>
+                            <Button
+                                sx={{ margin: "15px" }}
+                                color="pink"
+                                variant="contained"
+                                onClick={handleOnClear}
+                            >
+                                Clear Filters
+                            </Button>
+                        </ThemeProvider>
+                    ) : null}
                 </div>
             </Modal>
             <SelectMulti
                 openMultiView={openMultiView}
                 handleClose={onCloseMultiView}
                 type={selectedType}
-                setSelectedFilterOptions={setSelectedFilterOptions}
+                setters={setters} //setters for cities,costs,ratings,parking..
+                clearMulti={clearMulti}
             />
         </div>
     );
 };
-
-//    openMultiView,
-// list,
-// labelName,
-// filterValue,
-// setter,
