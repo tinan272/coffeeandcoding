4869a63fe7977ccb425b04bbe056278734c8a0f2
tinan272 <mnguyen354@gatech.edu>
2024-08-24 03:21:53 -0400

    fix about us

--- coffee_and_coding/app/frontend/components/Header.jsx
+++ coffee_and_coding/app/frontend/components/Header.jsx
@@ -4,45 +4,62 @@ import Link from "next/link";
 import { useMediaQuery } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import CloseIcon from "@mui/icons-material/Close";
+import { Backdrop } from "@mui/material";
+import MobileMenu from "../components/MobileMenu";
 
 export default function Header({ title, links, img }) {
     const [menuOpen, setMenuOpen] = useState(false);
     const isMobile = useMediaQuery("(max-width:768px)");
 
     return (
-        <div className="flex text-white justify-center w-full">
+        <div className="flex text-white justify-center w-full h-full">
             <div
                 id="dark-blue-fill"
-                className="flex flex-col md:flex-row h-auto md:h-88 top-0 w-full p-4 md:p-24 justify-between items-center md:items-baseline"
+                className="flex flex-col md:flex-row h-full md:h-88 top-0 w-full p-10 md:p-24 justify-between items-center md:items-baseline"
             >
                 <div className="flex justify-between items-center w-full md:w-auto">
-                    <div id="title" className="font-light text-2xl md:text-4xl">
+                    <div id="title" className="font-light text-3xl md:text-4xl">
                         {title}
                     </div>
-                    {isMobile && (
-                        <button
-                            onClick={() => setMenuOpen(!menuOpen)}
-                            className="text-white"
-                        >
-                            {menuOpen ? <CloseIcon /> : <MenuIcon />}
-                        </button>
-                    )}
+                    <div>
+                        {isMobile && (
+                            <button
+                                onClick={() => setMenuOpen(!menuOpen)}
+                                className="text-white"
+                            >
+                                {menuOpen ? null : <MenuIcon />}
+                            </button>
+                        )}
+                    </div>
                 </div>
-                <div
-                    className={`${
-                        isMobile && !menuOpen ? "hidden" : "flex"
-                    } flex-col md:flex-row font-thin text-lg md:text-xl mt-4 md:mt-0 text-center`}
-                >
-                    {links.map((link, index) => (
-                        <Link
-                            key={index}
-                            href={link.href}
-                            className="justify-between linkEffect linkEffect--insideOut my-2 md:my-0 md:mx-6"
-                            onClick={() => setMenuOpen(false)}
-                        >
-                            {link.name}
-                        </Link>
-                    ))}
+                <div className="text-lg md:text-xl">
+                    {!isMobile &&
+                        links.map((link, index) => (
+                            <Link
+                                key={index}
+                                href={link.href}
+                                className="flex justify-between linkEffect linkEffect--insideOut my-2 md:my-0 md:mx-6"
+                                onClick={() => setMenuOpen(false)}
+                            >
+                                {link.name}
+                            </Link>
+                        ))}
+                </div>
+            </div>
+            <div className="flex-col md:flex-row font-thin text-lg md:text-xl mt-4 md:mt-0 text-center">
+                <div>
+                    <Backdrop
+                        sx={{
+                            color: "#fff",
+                            zIndex: (theme) => theme.zIndex.drawer + 1,
+                            minHeight: "100vw",
+                            maxHeight: "100vh",
+                        }}
+                        open={menuOpen}
+                        onClick={() => setMenuOpen(!menuOpen)}
+                    >
+                        <MobileMenu title={title} links={links} />
+                    </Backdrop>
                 </div>
             </div>
         </div>
